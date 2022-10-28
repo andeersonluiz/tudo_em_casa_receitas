f25d90eddd456bd4ff8e19ac346ad33299085268
Anderson Luiz Bernardes da Rocha <andeersonluiz1998@gmail.com>
2022-10-28 03:26:01 -0300

    Revert "Add files via upload"
    This reverts commit 9ba88103a3fbfada660ba0677b42bac655674797.

--- support/constants.dart
+++
@@ -1,8 +0,0 @@
-class Constants {
-  // ignore: non_constant_identifier_names
-  static String HOME = "Inicio";
-  // ignore: non_constant_identifier_names
-  static String PANTRY = "Despensa";
-  // ignore: non_constant_identifier_names
-  static String RECIPES = "Receitas";
-}
